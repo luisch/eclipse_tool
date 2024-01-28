26732a8215a6e1f0a37b32e1bfdd925481a6ed25
luisch <lumino@mint>
2024-01-28 19:21:38 +0900

    add timechange script

---
+++ settime-20240408CDT.bat
@@ -0,0 +1,12 @@
+@echo off
+net session >NUL 2>nul
+if %errorlevel% neq 0 (
+ @powershell start-process %~0 -verb runas
+ exit
+)
+
+sc stop W32Time
+sc config "W32Time" start=disabled
+tzutil /s "Central Standard Time"
+date 2024/04/08
+time 12:18
